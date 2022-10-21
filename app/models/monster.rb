3467b84dd2f4c8d72a2aed30a15e41263f0f667a
Mariano Rodriguez <rodriguezmesamariano@gmail.com>
2022-10-21 12:50:13 -0300

    Terminado.

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,6 +1,6 @@
 class Monster < ApplicationRecord
     has_many :tweets, dependent: :destroy
-    validate: phone_number_valid_and_argentina
+    validate :phone_number_valid_and_argentina
 
     def phone_number_valid_and_argentina
         if PhoneValidator.new(phone).valid?
--- app/models/phone_validator.rb
+++ app/models/phone_validator.rb
@@ -3,14 +3,14 @@ class PhoneValidator
 
     base_uri "https://phonevalidation.abstractapi.com/v1/"
 
-    def initialize (number)
+    def initialize(number)
         @number = number
     end
 
     def valid?
         response = self.class.get("/", {
             query: {
-                api_key: Rails.application.credentials.abstract_api_key,
+                api_key: Rails.application.credentials.abstractapi_api_key,
             phone: @number
             }
             }
